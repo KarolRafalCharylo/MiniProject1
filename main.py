439d8be0d2c549c73066b3c4824caf88f0df4227
dKenez <47757462+dKenez@users.noreply.github.com>
2022-10-10 17:23:59 +0200

    Stacked cubes can be grabbed aswell

--- main.py
+++ main.py
@@ -21,12 +21,26 @@ from move_to import move_to_joint, move_to_cartesian
 
 roslib.load_manifest('hello_ros')
 
+
+class Coord:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+class Pos:
+    def __init__(self, x: int, y, z):
+        self.position = Coord(x, y, z)
+
+
 if __name__ == '__main__':
     try:
         rospy.init_node("miniproject1")
-        
+
         model_states = fetch_model_states()
-        cubes = extract_by_name(model_states, starts_with="cube", reversed=True)
+        cubes = extract_by_name(
+            model_states, starts_with="cube", reversed=True)
         bucket = extract_by_name(model_states, starts_with="bucket")[0]
         bucket[1].position.z = 1.3
         bucket = bucket[1]
@@ -34,33 +48,26 @@ if __name__ == '__main__':
         moveit_commander.roscpp_initialize(sys.argv)
         for i in range(len(cubes)):
             model_states = fetch_model_states()
-            cubes = extract_by_name(model_states, starts_with="cube", reversed=True)
+            cubes = extract_by_name(
+                model_states, starts_with="cube", reversed=True)
             print('Attempting pickup of {}'.format(cubes[i][0]))
             open()
             rospy.sleep(1)
-            
-            above_cube = cubes[i][1]
-            print("above_cube.position.z")
-            print(above_cube.position.z)
-            cube_pos_z = above_cube.position.z
 
-            above_cube.position.z = 1.1
+            cube_pos = Pos(cubes[i][1].position.x, cubes[i]
+                           [1].position.y, cubes[i][1].position.z)
 
-
-
-            move_to_joint(above_cube)
+            move_to_joint(Pos(cube_pos.position.x, cube_pos.position.y, 1.1))
             the_cube = cubes[i][1]
             print("old pos")
-            print(the_cube.position.z)
-            the_cube.position.z = cube_pos_z + 0.12
-            print("new pos")
-            print(the_cube.position.z)
-            move_to_cartesian(the_cube)
+            print(cube_pos.position.z)
+            move_to_cartesian(Pos(cube_pos.position.x, cube_pos.position.y, cube_pos.position.z + 0.17))
             close()
             rospy.sleep(1)
             above_cube = cubes[i][1]
             above_cube.position.z = 1.3
-            move_to_cartesian(above_cube)
+            move_to_cartesian(
+                Pos(cube_pos.position.x, cube_pos.position.y, 1.3))
             move_to_cartesian(bucket)
             #move_to_cartesian(above_cube, bucket)
             open()
